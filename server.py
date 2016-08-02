d932dfb54e2f4b9e3c26939bbdb210d4f117febf
Robert Czinege <czinege.robert@gmail.com>
2016-08-02 13:51:11 +0200

    Adding new element to the list with phony data works.

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+meal_list = [{'id': 13, 'text': 'Krumpli', 'calories': 300, 'date': '2016-01-01'},
+    {'id': 14, 'text': 'Kacsa', 'calories': 400, 'date': '2016-02-02'},
+    {'id': 15, 'text': 'Beka', 'calories': 200, 'date': '2016-08-01'}]
 
+ok_status = [{'status': 'OK'}]
 
 @app.route("/")
 def index():
@@ -9,9 +13,13 @@ def index():
 
 @app.route("/list", methods=['GET'])
 def get_all_food():
-    return jsonify([{'id': 13, 'text': 'Krumpli', 'calories': 300, 'date': '2016-01-01'},
-    {'id': 14, 'text': 'Kacsa', 'calories': 400, 'date': '2016-02-02'},
-    {'id': 15, 'text': 'Beka', 'calories': 200, 'date': '2016-08-01'}])
+    return jsonify(meal_list)
+
+@app.route("/list", methods=['POST'])
+def add_new_food():
+    meal_list.append({'id': 100, 'text': 'TEST MEAL', 'calories': 100, 'date': '2016-01-01'})
+    print(meal_list)
+    return jsonify(ok_status)
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=3000)
